036e801a84309eb62367453d61f843494c45b143
foundsunidhi <sunidhimundra05@gmail.com>
2024-11-19 16:42:12 +0530

    edit keypad colors

--- lib/widgets/calculator_keypad.dart
+++ lib/widgets/calculator_keypad.dart
@@ -1,4 +1,3 @@
-import 'package:calculator_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:calculator_app/constants/button_values.dart';
 import 'package:calculator_app/widgets/calculator_button.dart';
@@ -16,40 +15,45 @@ class CalculatorKeypad extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
 
     return Wrap(
-      children: Btn.buttonValues
-        .map(
+      children: Btn.buttonValues.map(
         (value) {
           Color backgroundColor;
           switch (value) {
-          case Btn.del:
-            backgroundColor = const Color(CColor.secondaryDarkColor);
-            break;
-          case Btn.clr:
-            backgroundColor = Colors.orange;
-            break;
-          case Btn.add:
-            backgroundColor = Colors.green;
-            break;
-          case Btn.subtract:
-            backgroundColor = Colors.blue;
-            break;
-          default:
-            backgroundColor = Colors.white;
+            case Btn.del:
+              backgroundColor = const Color.fromARGB(255, 196, 131, 33);
+              break;
+            case Btn.clr:
+              backgroundColor = const Color.fromARGB(255, 196, 131, 33);
+              break;
+            case Btn.add:
+              backgroundColor = Colors.yellow;
+              break;
+            case Btn.subtract:
+              backgroundColor = Colors.yellow;
+              break;
+            case Btn.multiply:
+              backgroundColor = Colors.yellow;
+              break;
+            case Btn.divide:
+              backgroundColor = Colors.yellow;
+              break;
+            case Btn.dot:
+              backgroundColor = const Color.fromARGB(255, 196, 131, 33);
+            default:
+              backgroundColor = Colors.white;
           }
           return SizedBox(
-          width: value == Btn.n0
-            ? screenSize.width / 2
-            : (screenSize.width / 4),
-          height: screenSize.width / 5,
-          child: CalculatorButton(
-            value: value,
-            onTap: () => onButtonTap(value),
-            backgroundColor: backgroundColor,
-          ),
+            width:
+                value == Btn.n0 ? screenSize.width / 2 : (screenSize.width / 4),
+            height: screenSize.width / 5,
+            child: CalculatorButton(
+              value: value,
+              onTap: () => onButtonTap(value),
+              backgroundColor: backgroundColor,
+            ),
           );
         },
-        )
-        .toList(),
+      ).toList(),
     );
   }
 }
